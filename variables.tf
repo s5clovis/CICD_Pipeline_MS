22f41ddf84fea213ea5c61a1f12a3c93fcd3ba1b
s5clovis <139711380+s5clovis@users.noreply.github.com>
2025-02-21 10:29:08 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,1 @@
 # variables.tf
-
-variable "dockerhub_username" {
-  description = "Docker Hub username"
-  type        = string
-  sensitive   = true
-}
-
-variable "dockerhub_password" {
-  description = "Docker Hub password"
-  type        = string
-  sensitive   = true
-}
